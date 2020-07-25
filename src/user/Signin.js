7c783af7a3822fb11be6f2ee8dcf5a28b1794e21
milek agrawal <milekag01@gmail.com>
2020-07-25 06:11:11 +0530

    auth refactoring

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import { signin, authenticate } from "../auth";
 
 class Signin extends Component {
     constructor() {
@@ -18,13 +19,6 @@ class Signin extends Component {
         this.setState({ [name]: event.target.value });
     };
 
-    authenticate(jwt, next) {
-        if (typeof window !== "undefined") {
-            localStorage.setItem("jwt", JSON.stringify(jwt));
-            next();
-        }
-    }
-
     clickSubmit = event => {
         event.preventDefault();
         this.setState({ loading: true });
@@ -34,31 +28,16 @@ class Signin extends Component {
             password
         };
         console.log(user);
-        this.signin(user).then(data => {
+        signin(user).then(data => {
             if (data.error) {
                 this.setState({ error: data.error, loading: false });
             } else {
                 // authenticate
-                this.authenticate(data, () => {
+                authenticate(data, () => {
                     this.setState({ redirectToReferer: true });
                 });
             }
         });
-    };
-
-    signin = user => {
-        return fetch("http://localhost:8080/signin", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(user)
-        })
-            .then(response => {
-                return response.json();
-            })
-            .catch(err => console.log(err));
     };
 
     signinForm = (email, password) => (
@@ -116,14 +95,13 @@ class Signin extends Component {
 
                 {loading ? (
                     <div className="jumbotron text-center">
-                        <h4>Loading...</h4>
+                        <h2>Loading...</h2>
                     </div>
                 ) : (
                     ""
                 )}
 
                 {this.signinForm(email, password)}
-
             </div>
         );
     }
