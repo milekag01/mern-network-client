aa49c6dc4c6001dd55bfe3bb71a77de9966cfef9
milek agrawal <milekag01@gmail.com>
2020-07-25 11:11:18 +0530

    add delete and edit user component

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { isAuthenticated } from "../auth";
 import { Redirect, Link } from "react-router-dom";
-import {read} from './apiUser';
+import { read } from "./apiUser";
+import DefaultProfile from "../images/avatar.jpg";
+import DeleteUser from "./DeleteUser";
 
 class Profile extends Component {
     constructor() {
@@ -12,21 +14,24 @@ class Profile extends Component {
         };
     }
 
-    
     init = userId => {
         const token = isAuthenticated().token;
-
-        read(userId,token)
-        .then(data => {
+        read(userId, token).then(data => {
             if (data.error) {
                 this.setState({ redirectToSignin: true });
             } else {
                 this.setState({ user: data });
             }
         });
-    }
+    };
+
     componentDidMount() {
         const userId = this.props.match.params.userId;
+        this.init(userId);
+    }
+
+    componentWillReceiveProps(props) {
+        const userId = props.match.params.userId;
         this.init(userId);
     }
 
@@ -36,30 +41,42 @@ class Profile extends Component {
 
         return (
             <div className="container">
+                <h2 className="mt-5 mb-5">Profile</h2>
                 <div className="row">
                     <div className="col-md-6">
-                        <h2 className="mt-5 mb-5">Profile</h2>
-                        <p>Hello {isAuthenticated().user.name}</p>
-                        <p>Email: {isAuthenticated().user.email}</p>
-                        <p>{`Joined ${new Date(
-                            user.created
-                        ).toDateString()}`}</p>
+                        <img
+                            className="card-img-top"
+                            src={DefaultProfile}
+                            alt={user.name}
+                            style={{
+                                width: "60%",
+                                height: "auto",
+                                objectFit: "cover"
+                            }}
+                        />
                     </div>
 
                     <div className="col-md-6">
-                        {isAuthenticated().user && isAuthenticated().user._id == user._id && (
-                            <div className="d-inline-block mt-5">
-                                <Link
-                                    className="btn btn-raised btn-success mr-5"
-                                    to={`/user/edit/${user._id}`}
-                                >
-                                    Edit Profile
-                                </Link>
-                                <button className="btn btn-raised btn-danger">
-                                    Delete Profile
-                                </button>
-                            </div>
-                        )}
+                        <div className="lead mt-2">
+                            <p>Hello {user.name}</p>
+                            <p>Email: {user.email}</p>
+                            <p>{`Joined ${new Date(
+                                user.created
+                            ).toDateString()}`}</p>
+                        </div>
+
+                        {isAuthenticated().user &&
+                            isAuthenticated().user._id == user._id && (
+                                <div className="d-inline-block">
+                                    <Link
+                                        className="btn btn-raised btn-success mr-5"
+                                        to={`/user/edit/${user._id}`}
+                                    >
+                                        Edit Profile
+                                    </Link>
+                                    <DeleteUser userId={user._id} />
+                                </div>
+                            )}
                     </div>
                 </div>
             </div>
