67ffa65f45b972b80eeefeb9053bba7628655ec6
milek agrawal <milekag01@gmail.com>
2020-07-26 15:25:50 +0530

    show user post on profile

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -6,6 +6,7 @@ import DefaultProfile from "../images/avatar.jpg";
 import DeleteUser from "./DeleteUser";
 import FollowProfileButton from "./FollowProfileButton";
 import ProfileTabs from "./ProfileTabs";
+import { listByUser } from "../post/apiPost";
 
 class Profile extends Component {
     constructor() {
@@ -14,7 +15,8 @@ class Profile extends Component {
             user: { following: [], followers: [] },
             redirectToSignin: false,
             following: false,
-            error: ""
+            error: "",
+            posts: []
         };
     }
 
@@ -49,6 +51,18 @@ class Profile extends Component {
             } else {
                 let following = this.checkFollow(data);
                 this.setState({ user: data, following: following});
+                this.loadPosts(data._id);
+            }
+        });
+    };
+
+    loadPosts = userId => {
+        const token = isAuthenticated().token;
+        listByUser(userId, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                this.setState({ posts: data });
             }
         });
     };
@@ -64,7 +78,7 @@ class Profile extends Component {
     }
 
     render() {
-        const { redirectToSignin, user } = this.state;
+        const { redirectToSignin, user, posts } = this.state;
         if (redirectToSignin) return <Redirect to="/signin" />;
 
         const photoUrl = user._id
@@ -77,7 +91,7 @@ class Profile extends Component {
             <div className="container">
                 <h2 className="mt-5 mb-5">Profile</h2>
                 <div className="row">
-                    <div className="col-md-6">
+                    <div className="col-md-4">
                         <img
                             className="card-img-top"
                             src={photoUrl}
@@ -91,7 +105,7 @@ class Profile extends Component {
                         />
                     </div>
 
-                    <div className="col-md-6">
+                    <div className="col-md-8">
                         <div className="lead mt-2">
                             <p>Hello {user.name}</p>
                             <p>Email: {user.email}</p>
@@ -103,12 +117,21 @@ class Profile extends Component {
                         {isAuthenticated().user &&
                             isAuthenticated().user._id == user._id ? (
                                 <div className="d-inline-block">
+
+                                    <Link
+                                        className="btn btn-raised btn-info mr-5"
+                                        to={`/post/create`}
+                                    >
+                                        Create Post
+                                    </Link>
+
                                     <Link
                                         className="btn btn-raised btn-success mr-5"
                                         to={`/user/edit/${user._id}`}
                                     >
                                         Edit Profile
                                     </Link>
+                                    
                                     <DeleteUser userId={user._id} />
                                 </div>
                             ) : (
@@ -130,6 +153,7 @@ class Profile extends Component {
                         <ProfileTabs
                             followers={user.followers}
                             following={user.following}
+                            posts={posts}
                         />
                         
                     </div>
